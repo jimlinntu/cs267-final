ce9f526c26207fe2cdd05e77f64e51e384d6f390
ByronHsu <byronhsu1230@gmail.com>
2022-04-06 19:37:36 -0400

    square cuda ddmm first commit

--- main.cu
+++ main.cu
@@ -6,69 +6,11 @@
 #include <cuda.h>
 #include <cusparse.h>
 #include <stdio.h>
-
+#include <iomanip>
 #include <assert.h>
 #include <iostream>
 
-struct Algo{
-    void spmm();
-    void sddmm();
-    void sddmm_spmm();
-};
-
-struct MatrixGenerator{
-    void generate_sparse_csr(int, int);
-    void generate_dense(int, int);
-};
-
-struct CusparseAlgo{
-    void spmm(cusparseHandle_t &handle,
-        cusparseSpMatDescr_t &S,
-        cusparseDnMatDescr_t &A, cusparseDnMatDescr_t &C);
-    void sddmm();
-    void sddmm_spmm();
-};
-
-// ---------------------------
-void Algo::spmm(){
-}
-
-void Algo::sddmm(){
-}
-
-void Algo::sddmm_spmm(){
-}
-
-void CusparseAlgo::spmm(
-        cusparseHandle_t &handle,
-        cusparseSpMatDescr_t &S,
-        cusparseDnMatDescr_t &A, cusparseDnMatDescr_t &C){
-
-    double alpha = 1.0, beta = 0.;
-    size_t bufsize = 0;
-    cusparseSpMM_bufferSize(handle,
-            CUSPARSE_OPERATION_NON_TRANSPOSE, CUSPARSE_OPERATION_NON_TRANSPOSE,
-            &alpha, S, A, &beta, C, CUDA_R_64F, CUSPARSE_SPMM_ALG_DEFAULT,
-            &bufsize);
-    void *dbuf = NULL;
-    assert(cudaMalloc(&dbuf, bufsize) == cudaSuccess);
-    assert(cusparseSpMM(handle,
-            CUSPARSE_OPERATION_NON_TRANSPOSE, CUSPARSE_OPERATION_NON_TRANSPOSE, 
-            &alpha, S, A, &beta, C, CUDA_R_64F, CUSPARSE_SPMM_ALG_DEFAULT, dbuf) == cudaSuccess);
-    assert(cudaFree(dbuf) == cudaSuccess);
-}
-
-void CusparseAlgo::sddmm(){
-}
-
-void CusparseAlgo::sddmm_spmm(){
-}
-
-void MatrixGenerator::generate_sparse_csr(int num_rows, int num_cols){
-}
-
-void MatrixGenerator::generate_dense(int num_rows, int num_cols){
-}
+#define TILE_WIDTH 2
 
 // forward declaration
 struct HostDenseMat;
@@ -85,6 +27,11 @@ struct HostDenseMat{
     ~HostDenseMat();
     void to_device(DeviceDenseMat &d);
     friend std::ostream& operator<<(std::ostream &os, const HostDenseMat &obj);
+    // overload set
+    double & operator [](int i) {return vals[i];}
+    // overload get
+    double operator [](int i) const {return vals[i];};
+    // overload comparison
 };
 
 struct DeviceDenseMat{
@@ -95,6 +42,10 @@ struct DeviceDenseMat{
 
     void get_cusparse_descriptor(cusparseDnMatDescr_t &mat);
     void copy_to_host(HostDenseMat &h);
+    // overload set
+    __device__ double & operator [](int i) {return vals[i];}
+    // overload get
+    __device__ double operator [](int i) const {return vals[i];};
 };
 
 struct HostSparseMat{
@@ -125,10 +76,140 @@ struct DeviceSparseMat{
     void get_cusparse_descriptor(cusparseSpMatDescr_t &mat);
 };
 
-// ==
-HostDenseMat::HostDenseMat(int num_rows_, int num_cols_, double *vals_)
-        :num_rows(num_rows_), num_cols(num_cols_), vals(vals_), to_delete(false){
+struct Algo{
+    void spmm();
+    void sddmm();
+    void sddmm_spmm();
+    void ddmm_seq(HostDenseMat &, HostDenseMat &, HostDenseMat &);
+    void ddmm(HostDenseMat &, HostDenseMat &, HostDenseMat &);
+};
 
+struct MatrixGenerator{
+    void generate_sparse_csr(int, int);
+    void generate_dense(int, int, double**);
+};
+
+struct CusparseAlgo{
+    void spmm(cusparseHandle_t &handle,
+        cusparseSpMatDescr_t &S,
+        cusparseDnMatDescr_t &A, cusparseDnMatDescr_t &C);
+    void sddmm();
+    void sddmm_spmm();
+};
+
+
+// ---------------------------
+void Algo::spmm(){
+}
+
+void Algo::sddmm(){
+}
+
+void Algo::sddmm_spmm(){
+}
+
+void Algo::ddmm_seq(HostDenseMat &A, HostDenseMat &B, HostDenseMat &C){
+    int A_num_rows = A.num_rows, A_num_cols = A.num_cols;
+    int B_num_rows = B.num_rows, B_num_cols = B.num_cols;
+    assert(A_num_cols == B_num_rows);
+
+    for(int i = 0; i < A_num_rows; i++) {
+        for(int j = 0; j < B_num_cols; j++) {
+            C[i*B_num_cols+j] = 0.0;
+            for(int k = 0; k < A_num_cols; k++) {
+                C[i*B_num_cols+j] += A[i*A_num_cols+k] * B[k*B_num_cols+j];
+            }
+        }
+    }
+}
+
+
+__global__ void ddmm_kernel(double* A, double* B, double* C, int width) {
+    // A, B, C are square
+    __shared__ double As[TILE_WIDTH][TILE_WIDTH];
+    __shared__ double Bs[TILE_WIDTH][TILE_WIDTH];
+
+    int gx_C = blockIdx.x * TILE_WIDTH + threadIdx.x;
+    int gy_C = blockIdx.y * TILE_WIDTH + threadIdx.y;
+    double value_C = 0.0;
+
+    for(int m = 0; m < width / TILE_WIDTH; m++) {
+        int lx_A = threadIdx.x;
+        int ly_A = threadIdx.y;
+        int gx_A = m * TILE_WIDTH + threadIdx.x;
+        int gy_A = gy_C;
+        As[ly_A][lx_A] = A[gy_A * width + gx_A];
+
+        int lx_B = threadIdx.x;
+        int ly_B = threadIdx.y;
+        int gx_B = gx_C;
+        int gy_B = m * TILE_WIDTH + threadIdx.y;
+        Bs[ly_B][lx_B] = B[gy_B * width + gx_B];
+        
+        __syncthreads();
+
+        for(int k = 0; k < TILE_WIDTH; k++)
+            value_C += As[ly_A][k] * Bs[k][lx_B];
+
+        __syncthreads(); // remove?
+
+    }
+
+    C[gy_C * width + gx_C] = value_C;
+}
+
+void Algo::ddmm(HostDenseMat &A, HostDenseMat &B, HostDenseMat &C){
+    DeviceDenseMat d_A, d_B, d_C;
+    A.to_device(d_A);
+    B.to_device(d_B);
+    C.to_device(d_C);
+
+    int width = A.num_rows;
+    dim3 dimGrid(width/TILE_WIDTH, width/TILE_WIDTH);
+    dim3 dimBlock(TILE_WIDTH, TILE_WIDTH);
+    ddmm_kernel<<<dimGrid, dimBlock>>>(d_A.vals, d_B.vals, d_C.vals, width);
+
+    d_C.copy_to_host(C);
+}
+
+void CusparseAlgo::spmm(
+        cusparseHandle_t &handle,
+        cusparseSpMatDescr_t &S,
+        cusparseDnMatDescr_t &A, cusparseDnMatDescr_t &C){
+
+    double alpha = 1.0, beta = 0.;
+    size_t bufsize = 0;
+    cusparseSpMM_bufferSize(handle,
+            CUSPARSE_OPERATION_NON_TRANSPOSE, CUSPARSE_OPERATION_NON_TRANSPOSE,
+            &alpha, S, A, &beta, C, CUDA_R_64F, CUSPARSE_SPMM_ALG_DEFAULT,
+            &bufsize);
+    void *dbuf = NULL;
+    assert(cudaMalloc(&dbuf, bufsize) == cudaSuccess);
+    assert(cusparseSpMM(handle,
+            CUSPARSE_OPERATION_NON_TRANSPOSE, CUSPARSE_OPERATION_NON_TRANSPOSE, 
+            &alpha, S, A, &beta, C, CUDA_R_64F, CUSPARSE_SPMM_ALG_DEFAULT, dbuf) == cudaSuccess);
+    assert(cudaFree(dbuf) == cudaSuccess);
+}
+
+void CusparseAlgo::sddmm(){
+}
+
+void CusparseAlgo::sddmm_spmm(){
+}
+
+void MatrixGenerator::generate_sparse_csr(int num_rows_, int num_cols_){
+}
+
+void MatrixGenerator::generate_dense(int num_rows_, int num_cols_, double** vals){
+    *vals = new double[num_rows_ * num_cols_];
+    for(int i = 0; i < num_rows_; i++)
+        for(int j = 0; j < num_cols_; j++)
+            (*vals)[i*num_cols_+j] = ((double)rand()/(double)RAND_MAX);
+}
+
+// ==
+HostDenseMat::HostDenseMat(int num_rows_, int num_cols_, double* vals_)
+        :num_rows(num_rows_), num_cols(num_cols_), vals(vals_), to_delete(false){
 }
 
 HostDenseMat::~HostDenseMat(){
@@ -143,10 +224,11 @@ void HostDenseMat::to_device(DeviceDenseMat &d){
     assert(cudaMalloc(&d.vals, num_rows * num_cols * sizeof(double)) == cudaSuccess);
     assert(cudaMemcpy(d.vals, vals, num_rows * num_cols * sizeof(double), cudaMemcpyHostToDevice) == cudaSuccess);
 }
+
 std::ostream& operator<<(std::ostream &os, const HostDenseMat &obj){
     for(int i = 0; i < obj.num_rows; ++i){
         for(int j = 0; j < obj.num_cols; ++j){
-            os << obj.vals[i*obj.num_cols + j] << "\t";
+            os << std::right << std::setw(4) << std::setprecision(2) << obj.vals[i*obj.num_cols + j] << "\t";
         }
         os << "\n";
     }
@@ -283,7 +365,43 @@ void test_spmm(){
     }
 }
 
+void test_ddmm() {
+    MatrixGenerator mg;
+    Algo alg;
+
+    // matrix A
+    int A_num_rows = 4, A_num_cols = 4;
+    double* A_vals = NULL;
+    mg.generate_dense(A_num_rows, A_num_cols, &A_vals);
+    HostDenseMat A(A_num_rows, A_num_cols, A_vals);
+
+    // matrix B
+    int B_num_rows = 4, B_num_cols = 4;
+    double* B_vals = NULL;
+    mg.generate_dense(B_num_rows, B_num_cols, &B_vals);
+    HostDenseMat B(B_num_rows, B_num_cols, B_vals);
+
+    // matrix C
+    int C_num_rows = A_num_rows, C_num_cols = B_num_cols;
+    double* C_vals = NULL;
+    mg.generate_dense(C_num_rows, C_num_cols, &C_vals);
+    HostDenseMat C(C_num_rows, C_num_cols, C_vals);
+
+    alg.ddmm_seq(A, B, C);
+    std::cout << C;
+
+    // matrix D
+    int D_num_rows = A_num_rows, D_num_cols = B_num_cols;
+    double* D_vals = NULL;
+    mg.generate_dense(D_num_rows, D_num_cols, &D_vals);
+    HostDenseMat D(D_num_rows, D_num_cols, D_vals);
+
+    alg.ddmm(A, B, D);
+    std::cout << D;
+}
+
 int main(){
-    test_spmm();
+    // test_spmm();
+    test_ddmm();
     return 0;
 }
